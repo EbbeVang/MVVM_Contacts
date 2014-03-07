c2525eb7c76a8686d5a842ded05702091d263741
ev <ev@evang.dk>
2014-03-07 10:29:48 +0100

    added project to git

---
+++ WpfApplication/ModelContact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// This is the model example in this small MVVM example
+    /// </summary>
+    class ModelContact
+    {
+        private string _firstname;
+        private string _lastname;
+        private string _email;
+
+        public String Firstname 
+        { 
+            get { return _firstname; }
+            set { _firstname = value; } 
+        }
+
+        public String LastName 
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        public String email {
+            get { return _email; }
+            set
+            {
+                if (value.Contains("@")) _email = value;
+            }
+        }
+    }
+}
---
+++ WpfApplication/ViewModelContact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplication.Annotations;
+
+namespace WpfApplication
+{
+    // extending INotifyPropertyChanged - means that i can notify when things change...
+    class ViewModelContact : INotifyPropertyChanged
+    {
+
+        #region automatically added when implementing INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+    }
+}
