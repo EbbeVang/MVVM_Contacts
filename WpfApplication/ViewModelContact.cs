4810612bbe473caed56b6a1ed6fb7db626a5a302
ev <ev@evang.dk>
2014-03-07 10:51:36 +0100

    exposed contacts in viewmodel

--- WpfApplication/ViewModelContact.cs
+++ WpfApplication/ViewModelContact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,6 +13,47 @@ namespace WpfApplication
     // extending INotifyPropertyChanged - means that i can notify when things change...
     class ViewModelContact : INotifyPropertyChanged
     {
+        // my bindings
+        private readonly String _currentContactName = "CurrentContact";
+        private readonly String _listOfContacts = "Contacts";
+
+        // my variables
+        private ModelContact _currentContact;
+        private List<ModelContact> _contacts = new List<ModelContact>(); 
+
+        // expose a contact to the UI
+        public ModelContact CurrentContact
+        {
+            get
+            {
+                return _currentContact;
+            }
+            set
+            {
+                //  only if user choose a new contact
+                if (value != _currentContact)
+                {
+                    _currentContact = value;
+                    OnPropertyChanged(_currentContactName);
+                }
+            }
+
+        }
+
+        //expose list of contacts to UI
+        public ObservableCollection<ModelContact> Contacts
+        {
+            get
+            {
+                ObservableCollection<ModelContact> contacts = new ObservableCollection<ModelContact>();
+                foreach (var modelContact in _contacts)
+                {
+                    contacts.Add(modelContact);
+                }
+                return contacts;
+            }
+            
+        }
 
         #region automatically added when implementing INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
